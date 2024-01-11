ba16a78c60661576c18fa7a6fe14091c1e6b4f9d
WJL <wangjunliang@buaa.edu.cn>
2024-01-11 17:20:04 +0800

    draw a plot by an excel
    add new draw (seaborn)

---
+++ draw.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+import pandas as pd
+import seaborn as sns
+
+#%% 从Excel中抽取数据
+
+x_name = "QP num"
+y_name = "Throuthput"
+
+df = pd.read_excel("ch2图表.xlsx", sheet_name=0, header=None, 
+                   names=[x_name, y_name], skiprows=[0, 1], usecols=[0, 1])
+x_line = df[x_name]
+y_line = df[y_name]
+
+#%% 绘制图形
+style_d = [y_name,]
+sns.lineplot(x=x_line, y=y_line, marker="o", color='black', 
+             markersize=8, linewidth=1).set(
+                 xlabel='Number of QPs', ylabel='Throuthput (Gb/s)')
+sns.set_theme(style='white', font_scale=1.1)
